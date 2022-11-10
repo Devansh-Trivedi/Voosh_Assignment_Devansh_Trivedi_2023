ca23f2524aa011523ef3cfb0e7acbbc982d18f1b
Devansh-Trivedi <tridevansh.160601@gmail.com>
2022-11-10 17:59:32 +0530

    project pushed

---
+++ server.js
@@ -0,0 +1,75 @@
+const express = require('express');
+const ejs = require('ejs');
+const path = require('path');
+const app = express();
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
+
+const MongoDBURI = process.env.MONGO_URI || 'mongodb://localhost/ManualAuth';
+
+mongoose.connect(MongoDBURI, {
+  useUnifiedTopology: true,
+  useNewUrlParser: true
+});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+});
+
+app.use(session({
+  secret: 'work hard',
+  resave: true,
+  saveUninitialized: false,
+  store: new MongoStore({
+    mongooseConnection: db
+  })
+}));
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(express.static(__dirname + '/views'));
+
+const index = require('./routes/index');
+app.use('/', index);
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  const err = new Error('File Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+// define as the last app.use callback
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  res.send(err.message);
+});
+
+// listen on port 3000
+app.listen(process.env.PORT || 3000, () => {
+  console.log('Express app listening on port 3000');
+});
+
+//Connecting to Hotel DB
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/hoteldb');
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+const routes = require('./api/routes/createOrderRoutes');
+routes(app);
+
+console.log('The CreateOrder-API server started on: ' + process.env.PORT || 3000);
+
+app.use((req, res) => {
+  res.status(404).send({url: req.originalUrl + ' not found'})
+});
